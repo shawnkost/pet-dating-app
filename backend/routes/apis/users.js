b6d564df3c0286bdc9eb3982588f308f39b604fb
Emily Sheng <emily.sheng@twisted-rope.com>
2022-11-19 12:41:55 -0800

    Create, update, and delete user routes

--- backend/routes/apis/users.js
+++ backend/routes/apis/users.js
@@ -69,4 +69,65 @@ router.post(
     }
 );
 
+/* @route  PUT api/users/:user_id/email
+ * @desc   update user email
+ * @access public
+ */
+router.put(
+    '/:user_id/email',
+    [auth, check('email', 'Email is required').not().isEmpty()],
+    async (req, res) => {
+        const errors = validationResult(req);
+        const userId = req.params.user_id;
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            User.countDocuments({ user: userId }, async (err, count) => {
+                if (count > 0) {
+                    const user = await User.findOne({ user: userId });
+
+                    user.email = email;
+                    await user.save();
+                    return res.json(user);
+                } else {
+                    return res.status(404).send('User not found');
+                }
+            });
+        } catch (err) {
+            console.log(err);
+            return res.status(500).send('Server error');
+        }
+    }
+);
+
+/* @route  DELETE api/users/:user_id
+ * @desc   update user email
+ * @access public
+ */
+router.delete('/:user_id', auth, async (req, res) => {
+    const errors = validationResult(req);
+    const userId = req.params.user_id;
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        User.countDocuments({ user: userId }, async (err, count) => {
+            if (count > 0) {
+                await User.deleteOne({ user: userId });
+                return res.json({ msg: 'User deleted' });
+            } else {
+                return res.status(404).send('User not found');
+            }
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send('Server error');
+    }
+});
+
 module.exports = router;
